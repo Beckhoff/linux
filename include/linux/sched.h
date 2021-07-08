3947389b629ca1a8432823f6d807db8a9f60d9f7
Ingo Molnar <mingo@kernel.org>
2021-07-08 14:49:42 +0200

    headers/deps: signals: Move task_struct::real_blocked to per_task()
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -661,7 +661,7 @@ struct task_struct {
 	/* Signal handlers: */
 	struct signal_struct		*signal;
 	struct sighand_struct __rcu		*sighand;
-	sigset_t			real_blocked;
+
 	/* Restored if set_restore_sigmask() was used: */
 	sigset_t			saved_sigmask;
 	struct sigpending		pending;
