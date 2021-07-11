c8bbd91c204aeffcd267b5504cf92471106316ef
Ingo Molnar <mingo@kernel.org>
2021-07-11 16:02:11 +0200

    headers/deps: Move task_struct::sched_task_group to per_task()
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -103,6 +103,10 @@ DECLARE_PER_TASK(int,					recent_used_cpu);
 DECLARE_PER_TASK(int,					wake_cpu);
 #endif
 
+#ifdef CONFIG_CGROUP_SCHED
+DECLARE_PER_TASK(struct task_group *,			sched_task_group);
+#endif
+
 struct rq;
 struct cpuidle_state;
 
@@ -1890,7 +1894,7 @@ static inline void flush_smp_call_function_from_idle(void) { }
  */
 static inline struct task_group *task_group(struct task_struct *p)
 {
-	return p->sched_task_group;
+	return per_task(p, sched_task_group);
 }
 
 /* Change a task's cfs_rq and parent entity if it moves across CPUs/groups */
