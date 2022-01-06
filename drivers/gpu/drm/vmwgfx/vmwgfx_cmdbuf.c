81f1b9d3349bdf1eb6c1b96c48a9898c1c8d149c
Ingo Molnar <mingo@kernel.org>
2022-01-06 13:26:17 +0100

    headers/deps: Add header dependencies to .c files: <linux/sched.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- drivers/gpu/drm/vmwgfx/vmwgfx_cmdbuf.c
+++ drivers/gpu/drm/vmwgfx/vmwgfx_cmdbuf.c
@@ -25,6 +25,7 @@
  *
  **************************************************************************/
 
+#include <linux/sched.h>
 #include <linux/workqueue_api.h>
 #include <linux/wait_api.h>
 #include <linux/spinlock_api.h>
