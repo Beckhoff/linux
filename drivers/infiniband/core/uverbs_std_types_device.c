33a4f440b59df8dea4b98f256ca6bc24b0b94dfc
Ingo Molnar <mingo@kernel.org>
2022-01-07 11:33:32 +0100

    headers/deps: Add header dependencies to .c files: <linux/netdevice_types.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- drivers/infiniband/core/uverbs_std_types_device.c
+++ drivers/infiniband/core/uverbs_std_types_device.c
@@ -3,6 +3,7 @@
  * Copyright (c) 2018, Mellanox Technologies inc.  All rights reserved.
  */
 
+#include <linux/netdevice_types.h>
 #include <linux/uaccess.h>
 #include <linux/spinlock_api.h>
 #include <linux/slab.h>
