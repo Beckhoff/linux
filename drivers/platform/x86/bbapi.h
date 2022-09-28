c84c696bc8a7f1716a7f8e5f83d243676dccc409
Patrick Bruenn <p.bruenn@beckhoff.com>
2022-09-28 10:37:43 +0200

    [BHF] platform/x86: add Beckhoff BBAPI driver
    The “BIOS-API” is a piece of software which is part of the BIOS in
    Beckhoff industrial motherboards. It offers a one-stop solution for
    communicating with several components on the board, such as
    temperature and voltage sensors, the S-USV microcontroller, the
    PWRCTRL microcontroller, the Watchdog and other components (if
    installed). It also offers access to a small memory area in the
    EEPROM reserved for user data. The API is integrated into the BIOS.
    The OS which is running on the board needs to have a special device
    driver installed to access the API functions. Through this driver
    the user software can take advantage of the API functionality.
    
    We rename bhf-bapi back to bbapi, because we derive the name for the
    character device from the module name. So we would change /dev/bbapi to
    /dev/bhf-bapi for no reason.
    Of course, we could change the code, but this would only make obvious
    how inconsistent our naming would be.
    
    Signed-off-by: Patrick Bruenn <p.bruenn@beckhoff.com>

---
+++ drivers/platform/x86/bbapi.h
@@ -0,0 +1,62 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Character Driver for Beckhoff BIOS API
+ * Author: Patrick Brünn <p.bruenn@beckhoff.com>
+ * Copyright (C) 2013 Beckhoff Automation GmbH & Co. KG
+ */
+
+#ifndef __API_H_
+#define __API_H_
+
+#undef pr_fmt
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/mutex.h>
+
+#define BBIOSAPI_SIGNATURE_PHYS_START_ADDR                                     \
+	0xFFE00000 // Defining the Physical start address for the search
+#define BBIOSAPI_SIGNATURE_SEARCH_AREA                                         \
+	0x001FFFFF // Defining the Memory search area size
+#define BBAPI_BUFFER_SIZE                                                      \
+	256 // maximum size of a buffer shared between user and kernel space
+
+struct simple_cdev {
+	dev_t dev; // First device number
+	struct cdev cdev; // Character device structure
+	struct class *class; // Device class
+};
+
+/**
+ * struct bbapi_object - manage access to Beckhoff BIOS functions
+ * @memory: pointer to a BIOS copy in RAM
+ * @entry: function pointer to the BIOS API function in RAM
+ * @in: buffer to exchange data between user space and BIOS
+ * @out: buffer to exchange data between BIOS and user space
+ * @dev: meta data for the character device interface
+ *
+ * The size of the output buffer should be large enough to satisfy the
+ * largest BIOS command. Right now this is: BIOSIOFFS_UEEPROM_READ.
+ */
+struct bbapi_object {
+	uint8_t *memory;
+	void *entry;
+	char in[BBAPI_BUFFER_SIZE];
+	char out[BBAPI_BUFFER_SIZE];
+	struct simple_cdev dev;
+	struct mutex mutex;
+};
+
+extern unsigned int bbapi_read(uint32_t group, uint32_t offset,
+			       void __kernel *out, uint32_t size);
+
+extern unsigned int bbapi_write(uint32_t group, uint32_t offset,
+				void __kernel *in, uint32_t size);
+
+extern unsigned int bbapi_rw(uint32_t group, uint32_t offset, void __kernel *in,
+			     uint32_t size_in, void __kernel *out,
+			     uint32_t size_out, uint32_t *bytes_written);
+
+extern int bbapi_board_is(const char *boardname);
+#endif /* #ifndef __API_H_ */
